c795465ab402bba180d574801a5dbf9a94a84f9c
James Campbell <Dr.J.Campbell@gmail.com>
2015-05-21 00:20:59 +0100

    An amlost usable analysis - correlations run and we get a table. The data.frame construction could do with a tidy

--- run_gdsc_analysis.r
+++ run_gdsc_analysis.r
@@ -60,20 +60,19 @@ comb_data_all_spearman_results <- data.frame(
 	drug=character(),
 	rho=numeric(),
 	pval=numeric(),
+	row.names=NULL,
 	stringsAsFactors=FALSE
 	)
-colnames(comb_data_all_spearman_results) <- c(
-	"marker",
-	"drug",
-	"rho",
-	"p.value"
-	)
+	
+r <- 0 # use to add index rows in the data.frame
+
 for(marker in colnames(comb_data$mutations)){
 	# marker now has the name of a column containing
 	# one of the mutated genes or genes altered by CNA
 	for(drug in colnames(comb_data$drugs)){
 		# likewise, drug is now the column name of
 		# one of the columns in the drugs table
+		r <- r + 1
 		result <- NULL
 		result <- cor.test(
 			comb_data$mutations[,marker],
@@ -81,21 +80,59 @@ for(marker in colnames(comb_data$mutations)){
 			method="spearman",
 			na.action=na.omit
 			)
-		comb_data_all_spearman_results <- rbind(
-			comb_data_all_spearman_results,
-			cbind(
-				marker,
-				drug,
-				result$estimate,
-				result$p.value
-				)
+		comb_data_all_spearman_results[r,] <- c(
+			marker,
+			drug,
+			result$estimate,
+			result$p.value
 			)
 	}
 }
 
+colnames(comb_data_all_spearman_results) <- c(
+	"marker",
+	"drug",
+	"rho",
+	"p.value"
+	)
+
+# now look at the following link for why this is a bad idea
+# http://architects.dzone.com/articles/some-tips-r-data-frames
+
+# sort out the types...
+comb_data_all_spearman_results <- data.frame(
+	marker=as.character(comb_data_all_spearman_results$marker),
+	drug=as.character(comb_data_all_spearman_results$drug),
+	rho=as.numeric(comb_data_all_spearman_results$rho),
+	p.value=as.numeric(comb_data_all_spearman_results$p.value),
+	stringsAsFactors=FALSE
+	)
 
 
+# volcano plot
+plot(
+	comb_data_all_spearman_results$rho,
+	-log10(comb_data_all_spearman_results$p.value)
+	)
 
+# which are the strongest associations (drug sensitivities)
+# r ≤ -0.1, p ≤ 0.05...
+comb_data_all_spearman_results[
+	which(
+		comb_data_all_spearman_results$rho >= 0.1 &
+		comb_data_all_spearman_results$p.value <= 0.001
+		)
+		,
+	]
+
+write.table(
+	comb_data_all_spearman_results,
+	file="comb_data_all_spearman_results.txt",
+	col.names=TRUE,
+	row.names=FALSE,
+	sep="\t",
+	quote=FALSE
+	)
 
 
 
