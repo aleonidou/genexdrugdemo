a6022c3aee6e36d2d69ee1375a1662e6f7ee003a
James Campbell <Dr.J.Campbell@gmail.com>
2015-05-21 11:00:37 +0100

    correlation analysis and plotting of mutation x IC50 working

--- run_gdsc_analysis.r
+++ run_gdsc_analysis.r
@@ -96,6 +96,16 @@ colnames(comb_data_all_spearman_results) <- c(
 	"p.value"
 	)
 
+write.table(
+	comb_data_all_spearman_results,
+	file="comb_data_all_spearman_results.txt",
+	col.names=TRUE,
+	row.names=FALSE,
+	sep="\t",
+	quote=FALSE
+	)
+
+
 # now look at the following link for why this is a bad idea
 # http://architects.dzone.com/articles/some-tips-r-data-frames
 
@@ -109,36 +119,95 @@ comb_data_all_spearman_results <- data.frame(
 	)
 
 
+# This should be changed for the actual demo but - for now
+# the results table has been split into CNV and mutation
+# predictors. Read in and visualise
+
+
+comb_data_mut_spearman_results <- read.table(
+	file="comb_data_mut_spearman_results.txt",
+	sep="\t",
+	header=TRUE,
+	stringsAsFactors=FALSE
+	)
+
+comb_data_cnv_spearman_results <- read.table(
+	file="comb_data_cnv_spearman_results.txt",
+	sep="\t",
+	header=TRUE,
+	stringsAsFactors=FALSE
+	)
+
+
+
+#
+# Visualise the test results
+#
+
 # volcano plot
 plot(
 	comb_data_all_spearman_results$rho,
 	-log10(comb_data_all_spearman_results$p.value)
 	)
 
-# which are the strongest associations (drug sensitivities)
-# r ≤ -0.1, p ≤ 0.05...
-comb_data_all_spearman_results[
-	which(
-		comb_data_all_spearman_results$rho >= 0.1 &
-		comb_data_all_spearman_results$p.value <= 0.001
-		)
-		,
-	]
 
-write.table(
-	comb_data_all_spearman_results,
-	file="comb_data_all_spearman_results.txt",
-	col.names=TRUE,
-	row.names=FALSE,
-	sep="\t",
-	quote=FALSE
+boxplot_drugIC50_by_mutation(
+	results=comb_data_mut_spearman_results[which(
+		comb_data_mut_spearman_results$p.value <= 0.0000001
+		),],
+	scores=comb_data$drugs,
+	mutations=comb_data$mutations,
+	filename="boxplots_drug_IC50_by_mutations.pdf"
 	)
 
 
-
-
-
-
-
-
-
+boxplot_drugIC50_by_mutation <- function(
+	results,
+	scores,
+	mutations,
+	filename
+	){
+	pdf(file=filename, width=2.5, height=3.5)
+	# turn off boxes for plots
+	par(bty="n", tcl=-0.2, mai=c(1, 0.95, 0.1, 0.1)) 
+	i <- NULL
+	for(i in 1:nrow(results)){
+		# start by setting all cell lines to wt
+		wt_mut_grps_strings <- rep(
+			"wt",
+			times=length(mutations[,results$marker[i]])
+			)
+		# set the recurrent/functional mutations
+		wt_mut_grps_strings[which(mutations[,results$marker[i]] == 1)] <- "mut"
+		wt_grp_rows <- which(wt_mut_grps_strings == "wt")
+		func_mut_grp_rows <- which(wt_mut_grps_strings == "mut")
+		# boxplot based on all data (wt and mut groups)
+		boxplot(
+			scores[wt_grp_rows,results$drug[i]],
+			scores[func_mut_grp_rows,results$drug[i]],
+			pch="",
+			names=c("wt", "mutant")
+			)
+		mtext(results$marker[i], 1, line=2, cex=0.8)
+		mtext("status", 1, line=3, cex=0.8)
+		mtext(results$drug[i], 2, line=3, cex=0.8)
+		mtext("log10 IC50 (µM)", 2, line=2, cex=0.8)
+		# jittrered points for each cell line
+		if(length(wt_grp_rows) > 0){
+			# plot at 1
+			points(
+				jitter(rep(1,times=length(wt_grp_rows)), amount=0.33),
+				scores[wt_grp_rows,results$drug[i]],
+				col=rgb(0,0,0,0.25),
+				pch=19
+				)
+		}
+		if(length(func_mut_grp_rows) > 0){
+			# plot at 2
+			points(
+				jitter(rep(2,times=length(func_mut_grp_rows)), amount=0.33), scores[func_mut_grp_rows,results$drug[i]], col=rgb(0,0,0,0.25), pch=19
+				)
+		}
+	}		
+	dev.off()
+}
